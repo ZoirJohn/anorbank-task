4e01b210a8d42a6c81c5e16a26826f81652267bf
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-06-16 16:16:33 +0500

    chore: remove netlify.toml and update pnpm-workspace.yaml with correct build options; enhance vite.config.ts by adding netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(),],
+  plugins: [tailwindcss(), reactRouter(),netlify()],
   resolve: {
     tsconfigPaths: true,
   },
