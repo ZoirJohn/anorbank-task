52bd22bc5c7c4f7b86585062ba0ca74a8ce138d1
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-06-16 16:22:58 +0500

    refactor: update Netlify plugin to use netlifyReactRouter for improved compatibility with React Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import netlify from "@netlify/vite-plugin";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(),netlify()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+	plugins: [tailwindcss(), reactRouter(), netlifyReactRouter()],
+	resolve: {
+		tsconfigPaths: true,
+	},
 });
